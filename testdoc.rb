68d5b63ad73c0ae1319ebe833f0cd6cfcf45444e
Dan Oliphant <danoliphant@mac.com>
2015-01-06 10:06:04 -0600

    Edit to testdoc.rb on master

--- testdoc.rb
+++ testdoc.rb
@@ -3,3 +3,10 @@
 # Practicing with branches and commits (1/6 9:33)
 
 # This comment made while in play-branch. (1/6 9:38)
+
+=begin
+This comment made as a corollary to the one made in play branch.
+I have edited testdoc.rb in both branches and will attempt a merge.
+Side note: I have also pushed the play-branch to github using terminal
+while keeping it part of the first-app project.
+=end
