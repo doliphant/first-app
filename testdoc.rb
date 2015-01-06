c0a46476e21ca50f6728a9acdd90cea43c9d87b8
Dan Oliphant <danoliphant@mac.com>
2015-01-06 09:53:02 -0600

    break test commit on play branch

--- testdoc.rb
+++ testdoc.rb
@@ -3,3 +3,8 @@
 # Practicing with branches and commits (1/6 9:33)
 
 # This comment made while in play-branch. (1/6 9:38)
+
+# This comment made in play branch after first merge and files match
+# Going to edit master branch with a different note like this one after
+# saving file in play branch. Then going to merge and see what happens.
+# Play branch merge test note: 123456789 (1/6 9:49)
